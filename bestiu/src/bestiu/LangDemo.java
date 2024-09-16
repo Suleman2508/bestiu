273a2976dc18d1a6e98cb625c6a9f6d6e6de18fa
ADMIN <ADMIN@DESKTOP-2E09PIH>
2024-09-17 00:08:00 +0530

    sixth commit

---
+++ bestiu/src/bestiu/LangDemo.java
@@ -0,0 +1,16 @@
+package bestiu;
+
+public class LangDemo {
+
+	public static void main(String[] args) {
+		String greeting="hello world";
+		System.out.println("Original:"  +greeting);
+		System.out.println("Upper case:" + greeting.toUpperCase());
+		
+		int max=Math.max(10,20);
+		System.out.println("Max:" + max);
+
+		
+	}
+
+}
