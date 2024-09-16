11868c349e586663fa9e5e7b09804953b522fd92
ADMIN <ADMIN@DESKTOP-2E09PIH>
2024-09-17 00:18:55 +0530

    sixth commit

---
+++ bestiu/src/bestiu/TypeCasting.java
@@ -0,0 +1,25 @@
+package bestiu;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		byte b=123;
+		int i=b;
+		System.out.println(i);
+		
+		float f=36.53f;
+		double d=f;
+		System.out.println(d);
+		
+		//explicit writing
+		 
+		double f1=76.35d;
+		long l=(long)f1;
+		System.out.println(l);
+		
+		float f2=876.39f;
+		int li=(int)f2;
+		System.out.println(li);
+	}
+
+}
