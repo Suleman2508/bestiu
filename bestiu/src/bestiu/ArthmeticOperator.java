5657de17bacc7dee500b0fbcb8c4f56745ad8b72
ADMIN <ADMIN@DESKTOP-2E09PIH>
2024-09-16 23:49:00 +0530

    sixth commit

---
+++ bestiu/src/bestiu/ArthmeticOperator.java
@@ -0,0 +1,25 @@
+package bestiu;
+
+public class ArthmeticOperator {
+
+	public static void main(String[] args) {
+int a=15, b=20;
+		
+		int sum=a+b;
+		System.out.println("Addition:" +sum);
+		
+		int diff =a-b; 
+		System.out.println("Subtraction : " + diff);
+	
+		int prod =a* b; 
+		System.out.println("Multiplication :" + prod);
+		
+		int quoitent =a/b; 
+		System.out.println("Division : " + quoitent);
+		
+		int remainder = a % b;
+		System.out.println("Remainder : " + remainder );
+
+	}
+
+}
