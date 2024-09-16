ebaf14b249088733e5b65f27bf34d74d8f0672ca
ADMIN <ADMIN@DESKTOP-2E09PIH>
2024-09-16 23:57:58 +0530

    sixth commit

--- bestiu/src/bestiu/DoWhile.java
+++ bestiu/src/bestiu/DoWhile.java
@@ -3,8 +3,13 @@ package bestiu;
 public class DoWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i = 1;
+		do {
+		    System.out.println(i);
+		    i++;
+		} while(i<=10);
 
+	
 	}
 
 }
