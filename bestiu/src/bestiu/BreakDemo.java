e46c29122002e237096921c37f1db858be58e867
ADMIN <ADMIN@DESKTOP-2E09PIH>
2024-09-16 23:52:11 +0530

    sixth commit

---
+++ bestiu/src/bestiu/BreakDemo.java
@@ -0,0 +1,15 @@
+package bestiu;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			if(i>=35) 
+			{
+				break;
+			}
+				System.out.println(i);
+		}
+	}
+
+}
