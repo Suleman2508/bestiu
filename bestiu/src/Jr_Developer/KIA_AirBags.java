59651967c9df3795c2fd4e5e40142f6b013121d7
ADMIN <ADMIN@DESKTOP-2E09PIH>
2024-09-17 12:40:48 +0530

    sixth commit

---
+++ bestiu/src/Jr_Developer/KIA_AirBags.java
@@ -0,0 +1,17 @@
+ package Jr_Developer;
+
+public class KIA_AirBags {
+
+	public void display_Air()
+	{
+       System.out.println("ALL KIA HAVE AIRBAGS");
+	}
+	public static void main(String[] args) {
+		  
+		  KIA_AirBags ob2=new KIA_AirBags();
+		  ob2.display_Air();
+	}
+}
+
+
+
