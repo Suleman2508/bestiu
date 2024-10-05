c10bc341e5414760a8efd504de93d65ecfdde27b
ADMIN <ADMIN@DESKTOP-2E09PIH>
2024-10-05 18:56:17 +0530

    15 ccommit

---
+++ bestiu/src/ArrayDemo/SingleDimArray.java
@@ -0,0 +1,17 @@
+package ArrayDemo;
+
+public class SingleDimArray {
+
+	public static void main(String[] args) {
+		int[] singlearray={1,2,3,4,5};
+		for (int i:singlearray)
+		{
+			System.out.print(i + "  ");
+		}
+
+	}
+
+
+	}
+
+
