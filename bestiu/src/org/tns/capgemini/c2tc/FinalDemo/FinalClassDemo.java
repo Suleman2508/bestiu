cbbbd8ccae6ff9682d13eab7d1950d0d4c79fd9a
ADMIN <ADMIN@DESKTOP-2E09PIH.lan>
2024-10-30 11:34:43 +0530

    11 COMMIT

---
+++ bestiu/src/org/tns/capgemini/c2tc/FinalDemo/FinalClassDemo.java
@@ -0,0 +1,22 @@
+package org.tns.capgemini.c2tc.FinalDemo;
+final class ImplementBysuleman
+{
+	public void show()
+	{
+		System.out.println("Ai integration done by Suleman");
+	}
+}
+class Umar extends ImplementBySuleman
+{
+	public void show()
+	{
+		System.out.println("Ai integration done by Umar");
+	}
+}
+public class FinalClassDemo {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ bestiu/src/org/tns/capgemini/c2tc/FinalDemo/FinalVar.java
@@ -0,0 +1,13 @@
+package org.tns.capgemini.c2tc.FinalDemo;
+
+
+public class Finalvar {
+
+	public static void main(String[] args) {
+		final int num=8;
+		num=9;
+		System.out.println(num);
+		
+	}
+
+}
